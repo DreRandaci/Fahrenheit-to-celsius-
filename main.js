415921d39b0b25d5eaa383e560df50a91c18a026
Dre mf Randaci <drerandaci@gmail.com>
2017-08-05 18:11:42 -0500

    added some things that don't work

--- main.js
+++ main.js
@@ -1,17 +1,57 @@
-console.log("JS linked")
+
+let userInput = document.getElementById("userInput").value;
+// userInput = parseFloat(userInput);
 
 function fahrenheitConverter(input) {
+	// debugger;
 	let fahrenheitOutput = (input-32) * 5/9;
 	fahrenheitOutput = fahrenheitOutput.toFixed(1);
-	return fahrenheitOutput + "°";
-}
+	// var fahrenheitString = fahrenheitOutput + "°F";
+    // return fahrenheitString;
+    console.log(fahrenheitOutput);
+    return fahrenheitOutput; //+ "°F";
+};
 
-// console.log(fahrenheitConverter(32))
+// console.log(fahrenheitConverter(0))
 
 function celsiusConverter(input) {
 	let celsiusOutput = input*9/5 + 32;
 	celsiusOutput = celsiusOutput.toFixed(1);
-	return celsiusOutput + "°";
-}
+	console.log(celsiusOutput);
+	return celsiusOutput; //+ "°C";
+};
 
-console.log(celsiusConverter(11))
+// console.log(celsiusConverter(0))
+
+function determineConverter(input) {
+	let radioSelectFahr = document.getElementById("fahrenheitRadio").checked;
+	let radioSelectCel = document.getElementById("celsiusRadio").checked;
+	let output = document.getElementById("output");
+	// radioSelect ? fahrenheitConverter() : celsiusConverter();
+	// debugger;
+	if (radioSelectFahr) {
+		return output.innerHTML = fahrenheitConverter(input);
+
+		// return output;
+	} else {
+	  	// output += '<h3>' + celsiusConverter(input) + '</h3>';
+	  	return output.innerHTML = celsiusConverter(input);
+	  	// return output;
+	};
+};
+// console.log(determineConverter());
+// console.log(userInput);
+// let convertedTemp = determineConverter();
+
+let convertButton = document.getElementById("convertButton").addEventListener('click', determineConverter(userInput));
+
+
+
+// output.innerHTML = determineConverter(userInput); 
+
+
+
+
+
+
+
